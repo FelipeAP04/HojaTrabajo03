cea7f20e0b3676d593c43467fec0905df53a2690
FelipeAP04 <agu23195@uvg.edu.gt>
2024-02-06 13:40:30 -0600

    Archivo para generar numeros aleatoreos

---
+++ GenerateNumbers.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GenerateNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    private static int[] generateNumbers(int size) {
+
+    }
+
+    private static void saveNumbersToFile(int[] numbers, String filename) {
+        
+    }
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+
+public class Main {
+    
+}
+
+Procedure gnomeSort(Array [] of Element)[
+    i=2;
+    while (i < n) do [
+        if A[i] > A[a 1] then;
+        i++;
+        else [
+            swap(a[i], a[a 1])
+        
+            ]
+]
+]
